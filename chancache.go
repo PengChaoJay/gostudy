171206ba0653aca0483bd9cea7142afa49bfa409
pengchaojay <zpcwyy@gmail.com>
2023-07-25 19:17:51 +0800

    添加了注释

--- chancache.go
+++ chancache.go
@@ -10,6 +10,7 @@ import (
 	"time"
 )
 
+// 创建任务
 const (
 	numberGoroutines = 4  // 要使用的 goroutine 的数量
 	taskLoad         = 10 // 要处理的工作的数量
@@ -39,7 +40,7 @@ func main() {
 	// 增加一组要完成的工作
 	for post := 1; post <= taskLoad; post++ {
 		tasks <- fmt.Sprintf("Task : %d", post)
-	}
+    }
 
 	// 当所有工作都处理完时关闭通道
 	// 以便所有 goroutine 退出
@@ -59,7 +60,7 @@ func worker(tasks chan string, worker int) {
 		// 等待分配工作
 		task, ok := <-tasks
 		if !ok {
-			// 这意味着通道已经空了，并且已被关闭
+            // 这意味着通道已经空了，并且已被关闭
 			fmt.Printf("Worker: %d : Shutting Down\n", worker)
 			return
 		}
@@ -67,6 +68,7 @@ func worker(tasks chan string, worker int) {
 		// 显示我们开始工作了
 		fmt.Printf("Worker: %d : Started %s\n", worker, task)
 
+        // 在进行这一步时候，线程只是挂起了，但是并没有关闭，因此在此步骤时候，线程休眠，休眠结束之后进行继续执行下面的
 		// 随机等一段时间来模拟工作
 		sleep := rand.Int63n(100)
 		time.Sleep(time.Duration(sleep) * time.Millisecond)
